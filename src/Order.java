28f7aab06d35d54927208be7ed368ed37b0e6d87
Caragh7 <morahancaragh@gmail.com>
2022-11-08 01:34:11 +0000

    creation of classes, initial set up of restaurant and table classes

--- src/Order.java
+++ src/Order.java
@@ -1,3 +1,50 @@
-public class Order
-{
+import java.util.ArrayList;
+
+public class Order {
+    private int tableID;
+    private ArrayList<FoodItem> newOrder;
+    private double billTotal;
+    private double tip;
+    private String orderStatus;
+
+    /**
+     * Constructs Order Object
+     */
+    public Order(int tableID) {
+        this.tableID = tableID;
+        newOrder = new ArrayList<FoodItem>();
+
+    }
+
+    public void addFood(FoodItem food) {
+        newOrder.add(food);
+
+    }
+
+    public void removeFood(FoodItem food) {
+        newOrder.remove(food);
+    }
+
+    public void getBillTotal() {
+        //getting total amount to pay from all food items in Order
+    }
+
+    public void setTip() {
+        // set tip to pay which will be added to bill when printed
+    }
+
+    public String toString() { //printing the bill method?
+        //print Bill total and tip as amount due to pay
+        return null;
+    }
+
+    public void setOrderStatus() {
+        /*
+        maybe the orderStatus should use an array of strings of statuses,
+         which I then change by incrementing i, after a wee timer of like
+         30 seconds 0.0
+         */
+    }
+//this is all I have for now plz dont judge >:(
+
 }
