94420a002e7b176ae995d0abdb5a4b1ab59dfcb1
Kamil <yes@gmail.com>
2022-11-09 19:11:29 +0000

    creation of classes, initial set up of restaurant and table classes

--- src/Waiter.java
+++ src/Waiter.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
+
 public class Waiter {
+
+    private int tableID;
+    private int customerID; /*dunno if this is needed */
+    private int waiterID; /* dunno if this is really needed */
+    private String status;
+    private ArrayList<FoodItem> Order;
+
+    public void newOrderFood(FoodItem food) {
+        Order.add(food);
+    }
+
+    public void removeFood(FoodItem food) {
+        Order.remove(food);
+    }
+
+    public void newOrder(int tableID, int customerID) {
+        this.tableID = tableID;
+        this.customerID = customerID;
+
+    }
+
+    public boolean setStatus(String status) {
+        if (status == "served") {
+            return true;
+        }
+    }
+
 }
