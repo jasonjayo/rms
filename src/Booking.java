f87092be1c788cda31c0c930a49b4ffbe86e530d
KamilM02 <kamilmrowiec02@gmail.com>
2022-11-10 18:28:36 +0000

    creation of classes, initial set up of restaurant and table classes

--- src/Booking.java
+++ src/Booking.java
@@ -1,3 +1,25 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Booking {
-    
+    private String name;
+    private LocalDateTime dateTime;
+    private int noOfPeople;
+
+    public void newBooking(String name, int noOfPeople) {
+        this.name = name;
+        this.dateTime = LocalDateTime.now();
+        this.noOfPeople = noOfPeople;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNoOfPeople() {
+        return noOfPeople;
+    }
+
 }
