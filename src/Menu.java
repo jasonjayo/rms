e4a7093834399243ef1cb216a3752caa8ded2c78
plutowootoe <adamgreenan30@gmail.com>
2022-11-10 18:28:11 +0000

    Menu methods added plus menus have been designed in menu.csv for each restaurant

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 public class Menu {
+
+    TreeMap<String, ArrayList<MenuCategory>> menu = new TreeMap<>();
+
+
+    public Menu(TreeMap<String, ArrayList<MenuCategory>> menu) {
+        this.menu = menu;
+    }
+
+    public TreeMap<String, ArrayList<MenuCategory>> getMenu() {
+        return menu;
+    }
 }
